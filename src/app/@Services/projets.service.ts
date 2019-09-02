32fd5363da5394e0276c94f4741ac0eab1a2a41a
GitHubOdix <azaheni@smartgraphe.com>
2019-09-02 14:38:57 +0100

    Add/Edit/GetAll Projets par idUtilisateur

--- src/app/@Services/projets.service.ts
+++ src/app/@Services/projets.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import * as jwt_decode from 'jwt-decode';
+
 import { UtilService } from '../@Util/util.service';
 import { Projet } from '../@Models/projet';
 import { Utilisateur } from '../@Models/utilisateur';
@@ -24,37 +23,37 @@ export class ProjetsService {
 
   constructor(private http: HttpClient, private utilService: UtilService) { }
 
-  // Retourne un tableau de tous les Projets : Projet[]
+  // Retourne un tableau de tous les Projets par idUtilisateur : Projet[]
   getAllProjetsService(): Observable<Projet[]> {
-    return this.http.get<Projet[]>(this.serviceUrl + '/allProjetsByIdUtilisateur/' + this.utilService.getIdUtilisateurFromToken());
+    const idUtilisateur = this.utilService.getIdUtilisateurFromToken();
+    return this.http.get<Projet[]>(this.serviceUrl + '/allProjetsByIdUtilisateur/' + idUtilisateur);
   }
 
-  // Retourne le Projet créé : Projet
+  // Retourne le Projet créé pour un utilisateur : Projet
   addProjetService(projet): Observable<Projet> {
-    // Je récupère l'idUtilisateur du Token pour l'envoyer dans les requêtes REST
-    const utilisateur = new Utilisateur();
-    utilisateur.id = jwt_decode(sessionStorage.getItem('token')).id;
-    const ProjetToStringify = projet;
-    // J'affecte l'Utilisateur au Projet avant de l'envoyer pour L'ajout du projet par idUtilisateur
-    ProjetToStringify.utilisateur = utilisateur;
-
+    const ProjetToStringify = this.addUtilisateurToProjet(projet);
     return this.http.post<any>(this.serviceUrl, JSON.stringify(ProjetToStringify), this.httpOptions);
   }
 
-  // Retourne le Projet modifié : Projet
+  // Retourne le Projet modifié pour un utilisateur : Projet
   editProjetService(projet): any {
-    // Je récupère l'idUtilisateur du Token pour l'envoyer dans les requêtes REST
-    const utilisateur = new Utilisateur();
-    utilisateur.id = jwt_decode(sessionStorage.getItem('token')).id;
-    const ProjetToStringify = projet;
-    // J'affecte l'Utilisateur au Projet avant de l'envoyer pour La modification du projet par idUtilisateur
-    ProjetToStringify.utilisateur = utilisateur;
-
+    const ProjetToStringify = this.addUtilisateurToProjet(projet);
     return this.http.put<any>(this.serviceUrl, JSON.stringify(ProjetToStringify), this.httpOptions);
   }
 
   // Ne retourne rien
   deleteProjetService(id) {
     return this.http.delete<any>(this.serviceUrl + '/' + id, this.httpOptions);
+  }
+
+  // Affecte un utilisateur à un projet et retourne le nouveau projet enrichi
+  addUtilisateurToProjet(projet) {
+    const utilisateur = new Utilisateur();
+    utilisateur.id = this.utilService.getIdUtilisateurFromToken();
+    const ProjetToStringify = projet;
+    // J'affecte l'Utilisateur au Projet avant de l'envoyer pour L'ajout du projet par idUtilisateur
+    ProjetToStringify.utilisateur = utilisateur;
+
+    return ProjetToStringify;
   }
 }
