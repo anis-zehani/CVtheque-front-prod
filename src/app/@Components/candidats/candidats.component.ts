41dfd12886625171bf1d8e12c1a8dbfa215e1700
GitHubOdix <azaheni@smartgraphe.com>
2019-09-05 17:14:56 +0100

    Privilèges : Gestion candidat & Gestion Opportunités

--- src/app/@Components/candidats/candidats.component.ts
+++ src/app/@Components/candidats/candidats.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit , ViewChild} from '@angular/core';
 import { DatagridCandidatsComponent } from '../../@Components/candidats/datagrid-candidats/datagrid-candidats.component';
-import { Technologie } from 'src/app/@Models/technologie';
 
+import { Technologie } from 'src/app/@Models/technologie';
+import { UtilService } from '../../@Util/util.service';
 
 @Component({
   selector: 'app-candidats',
@@ -11,43 +12,42 @@ import { Technologie } from 'src/app/@Models/technologie';
 
 export class CandidatsComponent implements OnInit {
 
-  //Afin d'appeler la fonction de Refrech Table chez le fils
+  // Afin d'appeler la fonction de Refrech Table chez le fils
   @ViewChild(DatagridCandidatsComponent, {static: false}) Datagrid: DatagridCandidatsComponent;
 
-  listeTechnologies : Technologie[];
+  listeTechnologies: Technologie[];
 
-  constructor() {}
+  role: string;
 
-  ngOnInit() {}
+  constructor(private utilService: UtilService) {}
 
-  //Afin de rafraichir la table quand on ajoute un Candidat
-  onRefreshTableEvent($event) 
-  {
+  ngOnInit() {
+    // je récupère le rôle pour la restriction d'accès dans le menu
+    this.role = this.utilService.getRoleUtilisateurFromToken();
+  }
+
+  // Afin de rafraichir la table quand on ajoute un Candidat
+  onRefreshTableEvent($event) {
     this.Datagrid.getAllCandidatsController(this.Datagrid.etatCandidat);
   }
 
-  //Afin de récupérer la liste des candidats par recherche via liste de technologies
-  onListeTechnologiesForSearchEvent($event)
-  {
-    
-    let listeTechnologiesFinale: Technologie[] = [];
+  // Afin de récupérer la liste des candidats par recherche via liste de technologies
+  onListeTechnologiesForSearchEvent($event) {
 
-    for(let i=0;i<$event.length;i++)
-    {
+    const listeTechnologiesFinale: Technologie[] = [];
+
+    for (let i = 0; i < $event.length; i++) {
       listeTechnologiesFinale.push($event[i]._value.id);
     }
     this.listeTechnologies = listeTechnologiesFinale;
 
 
-    if(this.listeTechnologies.length >0 )
-    {
+    if (this.listeTechnologies.length > 0 ) {
       this.Datagrid.getAllCandidatsByListTechnologiesController(this.listeTechnologies);
-    }
-    else
-    {
+    } else {
       this.Datagrid.getAllCandidatsController(this.Datagrid.etatCandidat);
     }
-    
+
   }
 
 }
--- src/app/@Components/opportunites/opportunites.component.ts
+++ src/app/@Components/opportunites/opportunites.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit , ViewChild} from '@angular/core';
 import { DatagridOpportunitesComponent } from '../../@Components/opportunites/datagrid-opportunites/datagrid-opportunites.component';
-import { Technologie } from 'src/app/@Models/technologie';
 
+import { Technologie } from 'src/app/@Models/technologie';
+import { UtilService } from '../../@Util/util.service';
 
 @Component({
   selector: 'app-opportunites',
@@ -11,44 +12,44 @@ import { Technologie } from 'src/app/@Models/technologie';
 
 export class OpportunitesComponent implements OnInit {
 
-  //Afin d'appeler la fonction de Refrech Table chez le fils
+  // Afin d'appeler la fonction de Refrech Table chez le fils
   @ViewChild(DatagridOpportunitesComponent, {static: false}) Datagrid: DatagridOpportunitesComponent;
 
-  listeTechnologies : Technologie[];
+  listeTechnologies: Technologie[];
   listeTechnologiesFinale: Technologie[] = [];
 
-  constructor() {}
+  role: string;
 
-  ngOnInit() {}
+  constructor(private utilService: UtilService) {}
 
-  //Afin de rafraichir la table quand on ajoute un Opportunite
-  onRefreshTableEvent($event) 
+  ngOnInit() {
+    // je récupère le rôle pour la restriction d'accès dans le menu
+    this.role = this.utilService.getRoleUtilisateurFromToken();
+  }
+
+  // Afin de rafraichir la table quand on ajoute un Opportunite
+  onRefreshTableEvent($event)
   {
     this.Datagrid.getAllOpportunitesController(this.Datagrid.etatOpportunite);
   }
 
-  //Afin de récupérer la liste des opportunités par recherche via liste de technologies
-  onListeTechnologiesForSearchEvent($event)
-  {
-    
-    let listeTechnologiesFinale: Technologie[] = [];
+  // Afin de récupérer la liste des opportunités par recherche via liste de technologies
+  onListeTechnologiesForSearchEvent($event) {
 
-    for(let i=0;i<$event.length;i++)
-    {
+    const listeTechnologiesFinale: Technologie[] = [];
+
+    for (let i = 0; i < $event.length; i++) {
       listeTechnologiesFinale.push($event[i]._value.id);
     }
     this.listeTechnologies = listeTechnologiesFinale;
 
 
-    if(this.listeTechnologies.length >0 )
-    {
+    if (this.listeTechnologies.length > 0 ) {
       this.Datagrid.getAllOpportunitesByListTechnologiesController(this.listeTechnologies);
-    }
-    else
-    {
+    } else {
       this.Datagrid.getAllOpportunitesController(this.Datagrid.etatOpportunite);
     }
-    
+
   }
 
 
