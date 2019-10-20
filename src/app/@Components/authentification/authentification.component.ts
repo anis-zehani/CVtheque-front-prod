cab609de7a1a29fc253c52f75fcccf9cc10fc6cc
GitHubOdix <azaheni@smartgraphe.com>
2019-10-20 23:44:58 +0100

    Création de compte Partenaire via HomePage

--- src/app/@Components/authentification/authentification.component.ts
+++ src/app/@Components/authentification/authentification.component.ts
@@ -39,7 +39,7 @@ export class AuthentificationComponent implements OnInit {
       dialogConfig.closeOnNavigation = true;
       const dialogRef = this.dialog.open(FormAddCompteCandidatComponent, {
         width: '500px',
-        height: '500px',
+        height: '400px',
         data: {
             // texte : "Attention : Afficher Message."
           }
