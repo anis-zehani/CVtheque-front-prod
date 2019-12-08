158bc21dc47606dba95d5b2cac5701b9149b4c62
Odix <azaheni@smartgraphe.com>
2019-12-08 22:32:46 +0100

    npm update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@ export const environment = {
   // storageUrl: 'http://localhost:9000'
 
   // Appels directement vers Gateway
-  // baseUrl: 'http://localhost:8080/api',
-  // storageUrl: 'http://localhost:9000'
+  baseUrl: 'http://localhost:8080/api',
+  storageUrl: 'http://localhost:9000'
 
   // Appel à la base PROD
-  baseUrl: 'http://plateforme.odix.fr:8090/api',
-  storageUrl: 'http://plateforme.odix.fr'
+  // baseUrl: 'http://plateforme.odix.fr:8090/api',
+  // storageUrl: 'http://plateforme.odix.fr'
 };
