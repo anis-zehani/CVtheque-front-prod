9f46ecd024f47a8c09f39b4e9608dc5125ee1ed8
Anis Zehani <contact@odix.fr>
2020-04-21 01:58:35 +0100

    Ajout Kubernetes.yml

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  baseUrl: 'http://gateway-ms:30080/api',
-  storageUrl: 'http://gateway-ms:30080'
+  production: false,
+  baseUrl: 'http://gateway-ms:30800/api',
+  storageUrl: 'http://gateway-ms:30800'
 };
