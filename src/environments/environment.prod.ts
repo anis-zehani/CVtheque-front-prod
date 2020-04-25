29f7713bff41f062b5f6f1e6a268aadc6a884829
Anis Zehani <contact@odix.fr>
2020-04-25 22:44:25 +0100

    mise à jour url gateway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://35.208.199.180:30080/api',
-  storageUrl: 'http://35.208.199.180:30080/'
+  baseUrl: 'http://ks8.odix.fr:30080/api',
+  storageUrl: 'http://ks8.odix.fr:30080'
 };
