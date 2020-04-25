8d7191a50c78f130feffe379f5b05ed81a775b9c
Anis Zehani <contact@odix.fr>
2020-04-25 03:44:44 +0100

    mise à jour gateway : crossorigin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://34.67.125.188:30080/api',
-  storageUrl: 'http://34.67.125.188:30080/'
+  baseUrl: 'http://35.208.199.180:30080/api',
+  storageUrl: 'http://35.208.199.180:30080/'
 };
