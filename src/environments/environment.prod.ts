7ef6631ea159e5f42958b9ec9b62509d7c1c73a2
GitHubOdix <azaheni@smartgraphe.com>
2020-07-30 17:08:12 +0100

    Credential ATS Monolithic

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   
   /* GCP Kubernetes Server*/
-  baseUrl: 'http://ks8.odix.fr:30080/api',
-  storageUrl: 'http://ks8.odix.fr'
+  //baseUrl: 'http://ks8.odix.fr:30080/api',
+  //storageUrl: 'http://ks8.odix.fr'
 
   /* Server Odix Smartgraphe*/
-  //baseUrl: 'http://ats.odix.fr:8090/api',
-  //storageUrl: 'http://ats.odix.fr'
+  baseUrl: 'http://ats.odix.fr:8090/api',
+  storageUrl: 'http://ats.odix.fr'
 };
